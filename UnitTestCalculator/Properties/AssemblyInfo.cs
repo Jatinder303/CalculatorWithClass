4661eab61817db1e924e051f852d339c9d054882
Jatinder Singh <Jatinder.Singh@atcltd.atc.org.nz>
2019-06-10 10:57:06 +1200

    updated

---
+++ UnitTestCalculator/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestCalculator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestCalculator")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6a56dce7-8934-4562-be69-2e35e98801d7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestCalculator/UnitTesting.cs
@@ -0,0 +1,93 @@
+﻿using CalculatorWithClass;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestCalculator
+{
+    [TestClass]
+    public class UnitTesting
+    {
+        //Arrange -  We need to make an instance of the Calulator class
+        Calulator cal1 = new Calulator();
+        string first = "3", second = "3";
+
+        [TestMethod]
+        public void TestAddPositive()
+        {
+            //Act - we need to provide some data to get an output
+            var Actual = cal1.Add(first, second);
+
+            //Assert - we need to check the output we get is correct or not
+            Assert.AreEqual(expected: "6", Actual);
+        }
+
+        [TestMethod]
+        public void TestAddNegative()
+        {
+            //Act - we need to provide some data to get an output
+            var Actual = cal1.Add(first, second);
+
+            //Assert - we need to check the output we get is correct or not
+            Assert.AreNotEqual(notExpected: "5", Actual);
+        }
+
+        [TestMethod]
+        public void TestsubPositive()
+        {
+            //Act - we need to provide some data to get an output
+            var Actual = cal1.Sub(first, second);
+
+            //Assert - we need to check the output we get is correct or not
+            Assert.AreEqual(expected: "0", Actual);
+        }
+
+        [TestMethod]
+        public void TestsubNegative()
+        {
+            //Act - we need to provide some data to get an output
+            var Actual = cal1.Sub(first, second);
+
+            //Assert - we need to check the output we get is correct or not
+            Assert.AreNotEqual(notExpected: "3", Actual);
+        }
+
+        [TestMethod]
+        public void TestMulPositive()
+        {
+            //Act - we need to provide some data to get an output
+            var Actual = cal1.Multi(first, second);
+
+            //Assert - we need to check the output we get is correct or not
+            Assert.AreEqual(expected: "9", Actual);
+        }
+
+        [TestMethod]
+        public void TestMulNegative()
+        {
+            //Act - we need to provide some data to get an output
+            var Actual = cal1.Multi(first, second);
+
+            //Assert - we need to check the output we get is correct or not
+            Assert.AreNotEqual(notExpected: "8", Actual);
+        }
+
+        [TestMethod]
+        public void TestDivPositive()
+        {
+            //Act - we need to provide some data to get an output
+            var Actual = cal1.Div(first, second);
+
+            //Assert - we need to check the output we get is correct or not
+            Assert.AreEqual(expected: "1", Actual);
+        }
+
+        [TestMethod]
+        public void TestDivNegative()
+        {
+            //Act - we need to provide some data to get an output
+            var Actual = cal1.Div(first, second);
+
+            //Assert - we need to check the output we get is correct or not
+            Assert.AreNotEqual(notExpected: "5", Actual);
+        }
+    }
+}
